fe9c6f4f5f25af9513ffde883a4a3741e138dd13
Kezia putri <Keziaputriayumagang@gmail.com>
2022-03-30 08:51:34 +0700

    Add header

---
+++ src/pages/Home.svelte
@@ -0,0 +1,21 @@
+<script>
+	import CharityList from "../components/CharityList.svelte";
+    
+	let title = "Charity";	
+	let charities = ['Charity 1', 'Charity 2', 'Charity 3']
+</script>
+
+<h1>{title}</h1>
+<CharityList />
+
+<style>
+h1 {
+	font-size: 3em;
+	color: #e2071c;
+}
+</style>
+
+<div>
+	<h1>{title}</h1>
+    <CharityList {charities} />
+</div>
